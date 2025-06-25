85fc25acba3f65a0597b691100f9979ad334fc02
bbalouki <bbalouki@outlook.com>
2025-06-25 10:49:07 +0000

    Enhance error messages display

--- strader/inputs.py
+++ strader/inputs.py
@@ -1,5 +1,6 @@
 import configparser
 from pathlib import Path
+from tkinter import messagebox
 from typing import Dict, List, Type
 
 from bbstrader.metatrader.trade import Trade, create_trade_instance
@@ -8,9 +9,12 @@ from bbstrader.metatrader.trade import Trade, create_trade_instance
 def load_config(filepath):
     config = configparser.ConfigParser(interpolation=None)
     if not Path(filepath).exists():
-        raise FileNotFoundError(f"Configuration file '{filepath}' not found.")
+        messagebox.showerror(
+            "Input Error", f"Configuration file '{filepath}' not found."
+        )
     config.read(filepath)
     return config
+
 
 def validate_input(input, input_type: Type, log_name: str):
     """
@@ -25,8 +29,9 @@ def validate_input(input, input_type: Type, log_name: str):
     try:
         return input_type(input)
     except Exception as e:
-        raise ValueError(
-            f"Invalid input for {log_name}: {e}, Must be of type {input_type.__name__}"
+        messagebox.showerror(
+            "Input Error",
+            f"Invalid input for {log_name}: {e}, Must be of type {input_type.__name__}",
         )
 
 
@@ -40,8 +45,10 @@ def get_tickers(tickers: str) -> Dict[str, str]:
     :raises ValueError: If the input string is empty or improperly formatted.
     """
     if not tickers:
-        raise ValueError("Tickers string cannot be empty.")
+        messagebox.showerror("Input Error", "Tickers string cannot be empty.")
     string = tickers.strip().replace("\n", "").replace(" ", "").replace('"""', "")
+    if string.endswith(","):
+        string = string[:-1]
     return dict(item.split(":") for item in string.split(","))
 
 
@@ -54,11 +61,13 @@ def get_mt5_path(path: str):
     :return: The validated path.
     :raises ValueError: If the path is invalid or does not point to 'terminal64.exe'.
     """
+    error = "MT5 Error"
     if not path or not Path(path).exists():
-        raise ValueError("Invalid MT5 path provided.")
+        messagebox.showerror(error, "Invalid MT5 path provided.")
     if not path.endswith("terminal64.exe"):
-        raise ValueError(
-            "MT5 path must end with 'terminal64.exe'. Please provide the correct path."
+        messagebox.showerror(
+            error,
+            "MT5 path must end with 'terminal64.exe'. Please provide the correct path.",
         )
     return path
 
@@ -72,5 +81,5 @@ def get_trade_instances(mt5_tickers: List[str], trade_args: Dict) -> Dict[str, T
     :return: A dictionary mapping each ticker to its corresponding Trade instance.
     """
     if not mt5_tickers:
-        raise ValueError("MT5 tickers list cannot be empty.")
+        messagebox.showerror("Input Error", "MT5 tickers list cannot be empty.")
     return create_trade_instance(mt5_tickers, trade_args)
